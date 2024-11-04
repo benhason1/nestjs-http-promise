9b7b885d78d399b6d025d9475bfbefb6ac25c913
Keith Wade <keith@keithwade.com>
2024-11-04 14:57:36 -0500

    Refactor HttpModule to use ConfigurableModuleBuilder

--- lib/http.module.ts
+++ lib/http.module.ts
@@ -1,114 +1,43 @@
-import { DynamicModule, Module, Provider } from '@nestjs/common';
+import {
+  Module,
+  ConfigurableModuleBuilder,
+  DynamicModule,
+} from '@nestjs/common';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+
 import Axios from 'axios';
-import {
-  AXIOS_INSTANCE_TOKEN,
-  HTTP_MODULE_ID,
-  HTTP_MODULE_OPTIONS,
-} from './http.constants';
-import { HttpService } from './http.service';
-import {
-  HttpModuleAsyncOptions,
-  HttpModuleOptions,
-  HttpModuleOptionsFactory,
-} from './interfaces';
 import axiosRetry from 'axios-retry';
 
-const createAxiosInstance = (config?: HttpModuleOptions) => {
-  const axiosInstance = Axios.create(config);
-  axiosRetry(axiosInstance, config);
-  return axiosInstance;
-}
+import { AXIOS_INSTANCE_TOKEN, HTTP_MODULE_ID } from './http.constants';
+import { HttpService } from './http.service';
+import { HttpModuleOptions } from './interfaces';
+
+const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
+  new ConfigurableModuleBuilder<HttpModuleOptions>().build();
 
 @Module({
   providers: [
     HttpService,
     {
       provide: AXIOS_INSTANCE_TOKEN,
-      useValue: createAxiosInstance(),
+      inject: [MODULE_OPTIONS_TOKEN],
+      useFactory: (options: HttpModuleOptions) => {
+        const axiosInstance = Axios.create(options);
+        axiosRetry(axiosInstance, options);
+        return axiosInstance;
+      },
+    },
+    {
+      provide: HTTP_MODULE_ID,
+      useValue: randomStringGenerator(),
     },
   ],
   exports: [HttpService],
 })
-export class HttpModule {
-  static register(config: HttpModuleOptions): DynamicModule {
-    return {
-      module: HttpModule,
-      providers: [
-        {
-          provide: AXIOS_INSTANCE_TOKEN,
-          useValue: createAxiosInstance(config),
-        },
-        {
-          provide: HTTP_MODULE_ID,
-          useValue: randomStringGenerator(),
-        },
-      ],
-    };
-  }
-
-  static registerAsync(options: HttpModuleAsyncOptions): DynamicModule {
-    return {
-      module: HttpModule,
-      imports: options.imports,
-      providers: [
-        ...this.createAsyncProviders(options),
-        {
-          provide: AXIOS_INSTANCE_TOKEN,
-          useFactory: (config: HttpModuleOptions) => createAxiosInstance(config),
-          inject: [HTTP_MODULE_OPTIONS],
-        },
-        {
-          provide: HTTP_MODULE_ID,
-          useValue: randomStringGenerator(),
-        },
-        ...(options.extraProviders || []),
-      ],
-    };
-  }
-
-  private static createAsyncProviders(
-      options: HttpModuleAsyncOptions,
-  ): Provider[] {
-    if (options.useExisting || options.useFactory) {
-      return [this.createAsyncOptionsProvider(options)];
-    }
-
-    const providers = [
-      this.createAsyncOptionsProvider(options)
-    ];
-
-    if(options.useClass)
-      providers.push({
-        provide: options.useClass,
-        useClass: options.useClass,
-      })
-
-    return providers;
-  }
-
-  private static createAsyncOptionsProvider(
-      options: HttpModuleAsyncOptions,
-  ): Provider {
-    if (options.useFactory) {
-      return {
-        provide: HTTP_MODULE_OPTIONS,
-        useFactory: options.useFactory,
-        inject: options.inject || [],
-      };
-    }
-
-    let inject;
-    if (options.useExisting)
-      inject = [options.useExisting];
-    else if (options.useClass)
-      inject = [options.useClass];
-
-    return {
-      provide: HTTP_MODULE_OPTIONS,
-      useFactory: async (optionsFactory: HttpModuleOptionsFactory) =>
-          optionsFactory.createHttpOptions(),
-      inject,
-    };
+export class HttpModule extends ConfigurableModuleClass {
+  // Redefining the register static method to allow `undefined` options to be
+  // accepted by the type system
+  public static register(options?: HttpModuleOptions): DynamicModule {
+    return super.register(options);
   }
 }
